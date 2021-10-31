14fc44f0611ccf3c3c43340ecb68699b63d30a44
Cassiana <lima.cglima@gmail.com>
2021-10-31 14:54:14 -0300

    inserindo uma lista de contatos no arquivo

--- testa_escrita.py
+++ testa_escrita.py
@@ -1,3 +1,9 @@
 arquivo_contatos = open("dados/contatos-escrita.csv", encoding="latin_1", mode="a")
-contato = "11,Carol,carol@carol.com.br\n"
-arquivo_contatos.write(contato)
+contatos = ["11,Carol,carol@carol.com.br\n",
+            "12,Ana,ana@ana.com.br\n",
+            "13,Tais,tais@tais.com.br\n",
+            "14,Felipe,felipe@felipe.com.br\n"]
+
+for contato in contatos:
+    arquivo_contatos.write(contato)
+
