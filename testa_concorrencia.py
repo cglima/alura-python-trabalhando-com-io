6c31ab22a9fa1646d8d323dfd0d065eccdfa2709
Cassiana <lima.cglima@gmail.com>
2021-11-08 15:53:20 -0300

    gerenciando o contexto de abertura do arquivo com 'with'

--- testa_concorrencia.py
+++ testa_concorrencia.py
@@ -1,8 +1,10 @@
-arquivo1 = open("dados.contatos-escrita.csv", encoding="latin_1", mode = "w")
-arquivo2 = open("dados.contatos-escrita.csv", encoding="latin_1", mode="w")
-
 contato_carol = "11,Carol,carol@carol.com.br\n"
 contato_andreza = "12,Andreza,andreza@andreza.com.br\n"
 
-arquivo1.write(contato_carol)
-arquivo2.write(contato_andreza)
+with open("dados.contatos-escrita.csv", encoding="latin_1", mode = "w") as arquivo1:
+    arquivo1.write(contato_carol)
+with open("dados.contatos-escrita.csv", encoding="latin_1", mode="w") as arquivo2:
+    arquivo2.write(contato_andreza)
+
+arquivo2.write('Nova linha')
+
