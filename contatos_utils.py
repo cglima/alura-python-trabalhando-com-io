092e54a7949a45767d11ef7b2458d39a5719f7d0
Cassiana <lima.cglima@gmail.com>
2021-11-08 17:31:43 -0300

    convertendo para json

--- contatos_utils.py
+++ contatos_utils.py
@@ -1,4 +1,4 @@
-import csv, pickle
+import csv, pickle, json
 from contato import Contato
 
 def csv_para_contatos(caminho, encoding="latin_1"):
@@ -19,4 +19,21 @@ def contatos_para_pickle(contatos, caminho):
 def pickle_para_contatos(caminho):
     with open(caminho, mode='rb') as arquivo:
         contatos = pickle.load(arquivo)
+    return contatos
+
+def contatos_para_json(contatos, caminho):
+    with open(caminho, mode ='w') as arquivo:
+        json.dump(contatos, arquivo, default=_contato_para_json)
+
+def _contato_para_json(contato):
+    return contato.__dict__
+
+def json_para_contatos(caminho):
+    contatos = []
+
+    with open(caminho) as arquivo:
+        contatos_json = json.load(arquivo)
+        for contato in contatos_json:
+            c = Contato(**contato)
+            contatos.append(c)
     return contatos
