e56fecf2bad98a7d5ab3fcae34b1371df15998af
Cassiana <lima.cglima@gmail.com>
2021-11-08 16:31:42 -0300

    escrita com buffer

--- testa_io.py
+++ testa_io.py
@@ -1,11 +1,13 @@
-arquivo = open ("dados/contatos.csv", encoding="latin_1")
+arquivo = open ("dados/contatos-escrita.csv", encoding="latin_1", mode="a+")
 
 print(type(arquivo.buffer))
 
-conteudo = arquivo.buffer.read()
-# print(conteudo)
 texto_em_bytes = bytes('Esse é um texto em bytes','latin_')
-print(texto_em_bytes)
-print(type(texto_em_bytes))
+# print(texto_em_bytes)
+# print(type(texto_em_bytes))
+
+contato = bytes('15,Verônica,veronica@veronica.com.br\n', 'latin_1')
+arquivo.buffer.write(contato)
+
 
 arquivo.close()
